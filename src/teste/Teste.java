a54969df0647e70650523ee228566be8d81b8a46
Tarlison <tarlison_brito@hotmail.com>
2020-06-09 01:19:18 -0400

    Prototipo da tabela 2 adicionado, esperando banco de dados para aplicar as funções de forma mais genérica

--- src/teste/Teste.java
+++ src/teste/Teste.java
@@ -11,26 +11,28 @@ import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Element;
 import com.itextpdf.text.Font;
-import com.itextpdf.text.Image;
+//import com.itextpdf.text.Image;
 import com.itextpdf.text.PageSize;
 import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.Phrase;
-import com.itextpdf.text.pdf.ColumnText;
+import com.itextpdf.text.Rectangle;
+//import com.itextpdf.text.pdf.ColumnText;
 import com.itextpdf.text.pdf.PdfPCell;
 import com.itextpdf.text.pdf.PdfPTable;
-import com.itextpdf.text.pdf.PdfPageEventHelper;
+//import com.itextpdf.text.pdf.PdfPageEventHelper;
 import com.itextpdf.text.pdf.PdfWriter;
-import java.io.File;
+//import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import org.jfree.chart.ChartFactory;
-import org.jfree.chart.ChartUtilities;
-import org.jfree.chart.JFreeChart;
-import org.jfree.chart.plot.PlotOrientation;
-import org.jfree.data.category.DefaultCategoryDataset;
+//import javafx.scene.layout.Border;
+//import org.jfree.chart.ChartFactory;
+//import org.jfree.chart.ChartUtilities;
+//import org.jfree.chart.JFreeChart;
+//import org.jfree.chart.plot.PlotOrientation;
+//import org.jfree.data.category.DefaultCategoryDataset;
 
 /**
  *
@@ -49,12 +51,13 @@ public class Teste {
 
             try {
                 
-                PdfWriter writer = PdfWriter.getInstance(documento, new FileOutputStream("PrimeiroPDF.pdf"));
+                PdfWriter writer = PdfWriter.getInstance(documento, new FileOutputStream("Bo_Epidemiologico.pdf"));
                 HeaderFooterPageEvent event = new HeaderFooterPageEvent();
                 writer.setPageEvent(event);
                 documento.open();
-                Font f1 = new Font(Font.FontFamily.TIMES_ROMAN, 16, Font.BOLD);
-                Font f2 = new Font(Font.FontFamily.TIMES_ROMAN, 14);
+                Font font1 = new Font(Font.FontFamily.UNDEFINED, 12, Font.BOLD);
+                Font font2 = new Font(Font.FontFamily.UNDEFINED, 10, Font.BOLD);
+                Font font3 = new Font(Font.FontFamily.UNDEFINED, 11, Font.BOLD);
                 
                 PdfPTable tbHeader = new PdfPTable(3);
                 PdfPCell _cell = new PdfPCell(new Paragraph("Cabeçalho"));
@@ -63,7 +66,7 @@ public class Teste {
                
                 documento.add(tbHeader);
                
-                Paragraph paragraph = new Paragraph("\n \n \n BOLETIM EPIDEMIOLÓGICO SOBRE A DOENÇA \n" + "PELO CORONAVÍRUS 2019 (COVID-19)", f1);
+                Paragraph paragraph = new Paragraph("\n \n \n BOLETIM EPIDEMIOLÓGICO SOBRE A DOENÇA \n"+ "PELO CORONAVÍRUS 2019 (COVID-19)", font1);
                 paragraph.setAlignment(Element.ALIGN_CENTER);
 
                 documento.add(paragraph);
@@ -79,7 +82,7 @@ public class Teste {
                         + "residência. Roraima – RR, 2020.\n\n");
                 teste.setAlignment(Element.ALIGN_JUSTIFIED);
                 documento.add(teste);
-
+                
                 PdfPTable tabela = new PdfPTable(5);
                 tabela.addCell("Municipios");
                 tabela.addCell("Notificados");
@@ -132,11 +135,91 @@ public class Teste {
                 tabela.addCell("13");
                 tabela.addCell("67");
                 documento.add(tabela);
-                Paragraph espaco = new Paragraph("\n \n \n \n \n \n");
+                
+                Paragraph espaco = new Paragraph("\n \n");
                 documento.add(espaco);
+               
+                
+                /*table 2 starts here*/
+                
+                PdfPTable table2 = new PdfPTable(7);
+                table2.setWidthPercentage(90);
+                table2.setWidths(new float[] { 3, 1, 1, 1, 1, 1, 1 });
+                PdfPCell _cellF, _cellM, _cellFe, _cellT;
+                
+                _cellF = new PdfPCell(new Phrase("FAIXA\u00a0ETÁRIA\u00a0", font3));
+                _cellF.setRowspan(2);
+                _cellF.setBorder(Rectangle.TOP | Rectangle.BOTTOM);
+                _cellF.setHorizontalAlignment(PdfPCell.ALIGN_CENTER);
+                table2.addCell(_cellF);
+          
+                
+                _cellM = new PdfPCell(new Phrase("MASCULINO", font3));
+                _cellM.setColspan(2);
+                _cellM.setBorder(Rectangle.TOP | Rectangle.BOTTOM);
+                _cellM.setHorizontalAlignment(PdfPCell.ALIGN_CENTER);
+                table2.addCell(_cellM);
+                
+                _cellFe = new PdfPCell(new Phrase("FEMININO", font3));
+                _cellFe.setColspan(2);
+                _cellFe.setBorder(Rectangle.TOP | Rectangle.BOTTOM);
+                _cellFe.setHorizontalAlignment(PdfPCell.ALIGN_CENTER);
+                table2.addCell(_cellFe);
+                
+                _cellT = new PdfPCell(new Phrase("TOTAL", font3));
+                _cellT.setColspan(2);
+                _cellT.setBorder(Rectangle.TOP | Rectangle.BOTTOM);
+                _cellT.setHorizontalAlignment(PdfPCell.ALIGN_CENTER);
+
+                table2.addCell(_cellT);
+                
+                String[] nAndPercent = {"nº", "%", "nº", "%", "Nº", "%"};
+                for (String iterator : nAndPercent){
+                    PdfPCell _cellNandPercent = new PdfPCell(new Phrase(iterator, font2));
+                    _cellNandPercent.setBorder(Rectangle.TOP | Rectangle.BOTTOM); //  esse
+                    _cellNandPercent.setHorizontalAlignment(PdfPCell.ALIGN_CENTER);// mais essa difere dos 2
+                    table2.addCell(_cellNandPercent);
+                }
+                
+                ///////////////////texto
+                PdfPCell _cellAux1 = new PdfPCell(new Phrase("Menor que 1 ano", font2));
+                _cellAux1.setBorder(PdfPCell.NO_BORDER);
+                _cellAux1.setHorizontalAlignment(PdfPCell.ALIGN_LEFT); //Difere dos 2
+                table2.addCell(_cellAux1);
+                //////////////////
+                 
+                String[] tableValues1 = {"34", "1,7", "28", "1,3", "62", "1,5"};
+                for (String iterator : tableValues1){
+                    PdfPCell _cellNandPercent = new PdfPCell(new Phrase(iterator, font2));
+                    _cellNandPercent.setBorder(PdfPCell.NO_BORDER); //Difere aqui dos 2
+                    _cellNandPercent.setHorizontalAlignment(PdfPCell.ALIGN_CENTER); //
+                    table2.addCell(_cellNandPercent);
+                }
+                
+                ///////////////////texto
+                PdfPCell _cellAux2 = new PdfPCell(new Phrase("1 a 9 anos", font2));
+                _cellAux2.setBorder(PdfPCell.NO_BORDER);
+                _cellAux2.setHorizontalAlignment(PdfPCell.ALIGN_LEFT);  //Difere dos 2
+                table2.addCell(_cellAux2);
+                //////////////////
+                    
+                String[] tableValues2 = {"67", "3,4", "67", "3,1", "134", "3,2"};
+                for (String iterator : tableValues2){
+                    PdfPCell _cellNandPercent = new PdfPCell(new Phrase(iterator, font2));
+                    _cellNandPercent.setBorder(PdfPCell.NO_BORDER); // Difere aqui dos 2
+                    _cellNandPercent.setHorizontalAlignment(PdfPCell.ALIGN_CENTER); //
+                    table2.addCell(_cellNandPercent);
+                }
+
+
+                
+
+                documento.add(table2);
+                
+                
                 documento.close();
                 writer.close();
-
+                
             } catch (DocumentException ex) {
                 Logger.getLogger(Teste.class.getName()).log(Level.SEVERE, null, ex);
             }
